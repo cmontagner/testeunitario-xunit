dc8b47b2cb435ab0d36890b6f119585639d6fc8e
caroline_montagner <caroline.montagner@salux.com.br>
2024-03-20 19:51:20 -0300

    Adicionando theory

--- ProjetoTestesXUnit/UnitTest1.cs
+++ ProjetoTestesXUnit/UnitTest1.cs
@@ -3,17 +3,29 @@ namespace ProjetoTestesXUnit
     public class ProjetoTestesXUnit_Testes
     {
         [Fact]
-        public void TestePassando()
+        //Facts testam condições invariantes (sempre verdadeiros)
+        public void FactPassando()
         {
-            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(4, Soma(2, 2));
         }
 
         [Fact]
-        public void TesteFalhando()
+        public void FactFalhando()
         {
-            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(5, Soma(2, 2));
         }
 
-        int Add(int a, int b) {  return a + b; }
+        [Theory]
+        //Theories testam condições que só serão verdadeiras para um determinado conjunto de dados
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void TheoryTeste(int valor)
+        {
+            Assert.True(EhImpar(valor));
+        }
+
+        int Soma(int a, int b) {  return a + b; }
+        bool EhImpar(int valor) { return valor % 2 == 1; }
     }
 }
